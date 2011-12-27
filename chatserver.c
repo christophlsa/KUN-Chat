32f0f319f41674a7aa997829b8aeff1ba7397796
Christoph Giesel <mail@cgiesel.de>
2011-12-27 18:01:35 +0100

    outsourcing of code lines in functions, bug fixing, removed unnecessary code, changed from nonblocking to blocking, use poll for new connections, user number != pollfd number, ...

--- chatserver.c
+++ chatserver.c
@@ -19,6 +19,7 @@ struct User
 
 int user_count = 0;
 int nick_count = 0;
+int poll_count = 1;
 struct pollfd* fds;
 struct User* user;
 
@@ -31,7 +32,7 @@ void sendToAll (struct User* user, char* msg)
 	printf(msg2send);
 
 	int i;
-	for (i = 0; i < user_count; i++)
+	for (i = 1; i < poll_count; i++)
 	{
 		write(fds[i].fd, msg2send, msg2sendsize-1);
 	}
@@ -61,18 +62,84 @@ void setNick (struct User* user, char* newnick)
 
 }
 
+struct User* findUserBySocketNumber (int socknum)
+{
+	int i;
+	for (i = 0; i < user_count; i++)
+	{
+		if (user[i].pollfd == socknum)
+		{
+			return &(user[i]);
+		}
+	}
+	
+	return NULL;
+}
+
+
+void handleNewConnection ()
+{
+	int peer_addr = accept(fds[0].fd, NULL, NULL);
+	
+	if (peer_addr < 0)
+	{
+		printf("Client Socket Error: %s\n", strerror(errno));
+		exit(1);
+	}
+	else
+	{
+		fds = (struct pollfd*) realloc(fds, sizeof(struct pollfd) * ++poll_count);
+		fds[poll_count-1].fd = peer_addr;
+		fds[poll_count-1].events = POLLIN;
+		
+		user = (struct User*) realloc(user, sizeof(struct User) * ++user_count);
+		user[user_count-1].pollfd = poll_count-1;
+		
+		setNick(&(user[user_count-1]), NULL);
+		
+		sendToAll(&(user[user_count-1]), "***connected to chat***\n");
+	}
+}
+
+void handleContent (struct User* user)
+{
+	if (user == NULL)
+	{
+		printf("user is NULL ?!?!\n");
+		exit(1);
+	}
+
+	char buffer[1025];
+	int readcount = read(fds[user->pollfd].fd, buffer, 1024);
+	buffer[readcount] = '\0';
+	
+	if (readcount < 0)
+	{
+		printf("Client Socket Error: %s\n", strerror(errno));
+		exit(1);
+	}
+	else if (readcount > 0)
+	{
+		if (strncmp(buffer, "/nick ", 6) == 0)
+		{
+			
+		}
+		else
+		{
+			sendToAll(user, buffer);
+		}
+	}
+}
+
 int main (int argc, char *argv[])
 {
-	signal(SIGCLD, SIG_IGN);
-
-	fds = (struct pollfd*) malloc(sizeof(struct pollfd) * 0);
+	fds = (struct pollfd*) malloc(sizeof(struct pollfd) * poll_count);
 	user = (struct User*) malloc(sizeof(struct User) * 0);
 
-	int sfd, peer_addr;
-	struct sockaddr_in my_addr, cfd;
-	socklen_t peer_addr_size = sizeof(peer_addr);
+	int sfd;
+	struct sockaddr_in my_addr;
 
-	sfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+	sfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (sfd == -1)
 	{
@@ -80,44 +147,24 @@ int main (int argc, char *argv[])
 		exit(1);
 	}
 	
+	int sockOpt = 1;
+	setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &sockOpt, sizeof(int));
+	
 	memset(&my_addr, 0, sizeof(struct sockaddr_in));
 
 	my_addr.sin_family = AF_INET;
 	my_addr.sin_addr.s_addr = INADDR_ANY;
 	my_addr.sin_port = htons(4444);
 
-	bind(sfd, (struct sockaddr*) &my_addr, sizeof(my_addr));
+	bind(sfd, (struct sockaddr*) &my_addr, sizeof(struct sockaddr_in));
 	listen(sfd, 5);
+
+	fds[0].fd = sfd;
+	fds[0].events = POLLIN;
 
 	while (1)
 	{
-		peer_addr = accept(sfd, (struct sockaddr*) &cfd, &peer_addr_size);
-		
-		if (peer_addr < 0)
-		{
-			if (errno != EAGAIN && errno != EWOULDBLOCK)
-			{
-				printf("Client Socket Error: %s\n", strerror(errno));
-				exit(1);
-			}
-		}
-		else
-		{
-			user_count++;
-			fds = (struct pollfd*) malloc(sizeof(struct pollfd) * user_count);
-			fds[user_count-1].fd = peer_addr;
-			fds[user_count-1].events = POLLOUT;
-			fds[user_count-1].revents = 0;
-			
-			user = (struct User*) realloc(user, sizeof(struct User) * user_count);
-			user[user_count-1].pollfd = user_count-1;
-			
-			setNick(&(user[user_count-1]), NULL);
-			
-			sendToAll(&(user[user_count-1]), "***connected to chat***\n");
-		}
-		
-		int pollr = poll(fds, user_count, 500);
+		int pollr = poll(fds, poll_count, 500);
 		
 		if(pollr < 0)
 		{
@@ -127,32 +174,17 @@ int main (int argc, char *argv[])
 		else if (pollr > 0)
 		{
 			int i;
-			for (i = 0; i < user_count; i++)
+			for (i = 0; i < poll_count; i++)
 			{
-				if (fds[i].revents & POLLOUT)
+				if (fds[i].revents & POLLIN)
 				{
-					char buffer[1025];
-					int readcount = read(fds[i].fd, buffer, 1024);
-					buffer[readcount] = '\0';
-					
-					if (readcount < 0)
+					if (i == 0)
 					{
-						if (errno != EAGAIN && errno != EWOULDBLOCK)
-						{
-							printf("Client Socket Error: %s\n", strerror(errno));
-							exit(1);
-						}
+						handleNewConnection();
 					}
-					else if (readcount > 0)
+					else
 					{
-						if (strncmp(buffer, "/nick ", 6) == 0)
-						{
-							
-						}
-						else
-						{
-							sendToAll(&(user[i]), buffer);
-						}
+						handleContent(findUserBySocketNumber(i));
 					}
 				}
 			}
